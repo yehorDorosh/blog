4436ec4ca360bd3ce30fe7e3bf60d9c2a6c3846b
Yehor <yehor.dorosh@ukr.net>
2024-07-20 13:19:11 +0200

    Add contrib text editor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,9 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(),
+    provideHttpClient(withFetch())
+  ],
 };
--- src/app/blog/article-editor/article-editor.component.ts
+++ src/app/blog/article-editor/article-editor.component.ts
@@ -1,40 +1,78 @@
-import { Component, inject, input, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ArticleService } from '../article.service';
-import { Validators } from '@angular/forms';
 import { BlogArticle } from '../blog.model';
+
+import { HttpClientModule} from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-article-editor',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [FormsModule, HttpClientModule, AngularEditorModule],
   templateUrl: './article-editor.component.html',
-  styleUrl: './article-editor.component.scss'
+  styleUrl: './article-editor.component.scss',
 })
 export class ArticleEditorComponent implements OnInit {
+  title: string = '';
+  content = '';
   private articleService = inject(ArticleService);
 
   article = input.required<BlogArticle>();
 
-  form = new FormGroup({
-    title: new FormControl('', { 
-      validators: [Validators.required],
-     }),
-    content: new FormControl('', {
-      validators: [Validators.required],
-    }),
-  });
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: 'auto',
+    minHeight: '0',
+    maxHeight: 'auto',
+    width: 'auto',
+    minWidth: '0',
+    translate: 'yes',
+    enableToolbar: true,
+    showToolbar: true,
+    placeholder: 'Enter text here...',
+    defaultParagraphSeparator: '',
+    defaultFontName: '',
+    defaultFontSize: '',
+    fonts: [
+      {class: 'arial', name: 'Arial'},
+      {class: 'times-new-roman', name: 'Times New Roman'},
+      {class: 'calibri', name: 'Calibri'},
+      {class: 'comic-sans-ms', name: 'Comic Sans MS'}
+    ],
+    customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+    uploadUrl: 'v1/image',
+    // upload: (file: File) => {  },
+    uploadWithCredentials: false,
+    sanitize: false,
+    toolbarPosition: 'top',
+    toolbarHiddenButtons: [
+      ['bold', 'italic'],
+      ['fontSize']
+    ]
+  };
 
-  ngOnInit() {
-    this.form.patchValue({
-      title: this.article().title,
-      content: this.article().content,
-    });
-  }
+  ngOnInit() {}
 
   onFileSelected(event: Event) {}
 
   onSubmit() {
-    console.log(this.form.value);
+    console.log(this.content);
   }
 }
