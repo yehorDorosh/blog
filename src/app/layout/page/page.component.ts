18504cbffbe5903b3ef53d74f9778d287b9efd9c
Yehor <yehor.dorosh@ukr.net>
2024-12-17 17:16:20 +0100

    Add footer

--- src/app/layout/page/page.component.ts
+++ src/app/layout/page/page.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { LangList } from '../../lang-switcher/lang-switcher.model';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss',
 })
