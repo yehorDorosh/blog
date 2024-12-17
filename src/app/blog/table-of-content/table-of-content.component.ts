422b97275e8ad29a630dd4bc4683711305ca8bc3
Yehor <yehor.dorosh@ukr.net>
2024-12-17 10:04:50 +0100

    Upd table of content

--- src/app/blog/table-of-content/table-of-content.component.ts
+++ src/app/blog/table-of-content/table-of-content.component.ts
@@ -3,6 +3,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { uid } from 'uid';
 import { RouterLink } from '@angular/router';
 import translit from '../../utils/translit';
+import { type Header } from './table-of-content.model';
 
 @Component({
   selector: 'app-table-of-content',
@@ -13,10 +14,7 @@ import translit from '../../utils/translit';
 })
 export class TableOfContentComponent {
   private platformId = inject(PLATFORM_ID);
-  public headers: {
-    textContent: string;
-    id: string;
-  }[] = [];
+  public headers: Header[] = [];
 
   ngAfterContentInit(): void {
     if (isPlatformBrowser(this.platformId)) {
@@ -28,8 +26,36 @@ export class TableOfContentComponent {
         this.headers.push({
           textContent: header.textContent || '',
           id,
+          headers: this.getSubTitles(header, 'H3').map((h3) => {
+            const id = translit(h3.textContent || uid());
+            h3.id = id;
+            return {
+              textContent: h3.textContent || '',
+              id,
+              headers: this.getSubTitles(h3, 'H4').map((h4) => {
+                const id = translit(h4.textContent || uid());
+                h4.id = id;
+                return {
+                  textContent: h4.textContent || '',
+                  id,
+                };
+              }),
+            };
+          }),
         });
       });
     }
+  }
+
+  getSubTitles(target: Element, tagName: string) {
+    const headings = [];
+    let sibling = target.nextElementSibling;
+    while (sibling) {
+      if (sibling.tagName === target.tagName) break;
+      if (sibling.tagName === tagName) headings.push(sibling);
+      sibling = sibling.nextElementSibling;
+    }
+
+    return headings;
   }
 }
---
+++ src/app/blog/table-of-content/table-of-content.model.ts
@@ -0,0 +1,5 @@
+export interface Header {
+  textContent: string;
+  id: string;
+  headers?: Header[];
+}
