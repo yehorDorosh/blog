f8604e4f6be4817b9fdd94388336d3e39765935e
Yehor <yehor.dorosh@ukr.net>
2024-12-20 15:07:02 +0100

    Change btns to links in lang switcher

--- src/app/lang-switcher/lang-switcher.component.ts
+++ src/app/lang-switcher/lang-switcher.component.ts
@@ -1,7 +1,18 @@
-import { Component, inject, signal, HostListener, input } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  HostListener,
+  input,
+  computed,
+  PLATFORM_ID,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { LangSwitcherService } from './lang-switcher.service';
 import { LangList } from './lang-switcher.model';
+import { isPlatformBrowser } from '@angular/common';
+import { REQUEST } from '../../express.tokens';
+import { Request } from 'express';
 
 @Component({
   selector: 'app-lang-switcher',
@@ -13,12 +24,26 @@ import { LangList } from './lang-switcher.model';
 export class LangSwitcherComponent {
   router = inject(Router);
   langSwitcherService = inject(LangSwitcherService);
+  private platformId = inject(PLATFORM_ID);
+  req = inject(REQUEST, { optional: true }) as Request;
+
   providedLangs = input<LangList[] | undefined>(
     this.langSwitcherService.langList
   );
+
   openSwitcher = signal(false);
 
-  changeLanguageHandler(lang: string) {
+  url = computed(() => {
+    if (isPlatformBrowser(this.platformId)) {
+      const path = window.location.pathname.split('/');
+      path.splice(1, 1);
+      return path.join('/');
+    }
+    return this.req?.url;
+  });
+
+  changeLanguageHandler(event: MouseEvent, lang: string) {
+    event.preventDefault();
     this.langSwitcherService.changeLanguage(lang);
   }
 
