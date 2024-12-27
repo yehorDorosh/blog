7f6addc3d88b0aaf0a48aed82e8485bb5bc31d34
Yehor <yehor.dorosh@ukr.net>
2024-12-27 15:44:32 +0100

    Disable HTTP2

--- src/proxy-server.mjs
+++ src/proxy-server.mjs
@@ -3,14 +3,15 @@ import fs from 'fs';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 import { IpFilter } from 'express-ipfilter';
-import spdy from 'spdy';
-import { app as serverEn } from './server/en/server.mjs';
-import { app as serverRu } from './server/ru/server.mjs';
-import { app as serverUk } from './server/uk/server.mjs';
+// import spdy from 'spdy';
+import https from 'https';
+import { app as appEn } from './server/en/server.mjs';
+import { app as appRu } from './server/ru/server.mjs';
+import { app as appUk } from './server/uk/server.mjs';
 
 function run() {
   const port = process.env.PORT || 4000;
-  const server = express();
+  const app = express();
 
   const limiter = rateLimit({
     windowMs: +(process.env['RATE_LIMIT_WINDOW'] || 10) * 60 * 1000,
@@ -20,15 +21,15 @@ function run() {
   });
   const ips = process.env['IP_BLACK_LIST']?.split(',') ?? [];
 
-  server.use('/uk', serverUk());
-  server.use('/ru', serverRu());
-  server.use('/en', serverEn());
-  server.use('/', serverEn());
+  app.use('/uk', appUk());
+  app.use('/ru', appRu());
+  app.use('/en', appEn());
+  app.use('/', appEn());
 
   if (process.env.NODE_ENV === 'production') {
-    server.use(IpFilter(ips, { log: false }));
-    server.use(limiter);
-    server.use(
+    app.use(IpFilter(ips, { log: false }));
+    app.use(limiter);
+    app.use(
       helmet({
         contentSecurityPolicy: {
           directives: {
@@ -49,14 +50,16 @@ function run() {
     };
 
     //spdy HTTP2
-    const httpsServer = spdy.createServer(credentials, server);
+    // const httpsServer = spdy.createServer(credentials, server);
+
+    const httpsServer = https.createServer(credentials, app);
 
     httpsServer.listen(port, () => {
       console.log(`Node HTTPS server listening on port ${port}`);
     });
   
   } else {
-    server.listen(port, () => {
+    app.listen(port, () => {
       console.log(`Node Express server listening on http://localhost:${port}`);
     });
   }
